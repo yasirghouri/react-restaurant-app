271173e13bfac6eff728f0872305e895083c00a1
yasirghouri <yasir.ag7@gmail.com>
2022-07-02 20:30:27 +0500

    completed header component with google auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
+import { AnimatePresence } from "framer-motion";
+import { Header } from "./components";
+
+console.log("NEW LINE", process.env.REACT_APP_FIREBASE_API_KEY);
+console.log("NEW LINE", process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
+console.log("NEW DB_URL", process.env.REACT_APP_FIREBASE_DB_URL);
+console.log("NEW LINE", process.env.REACT_APP_FIREBASE_PROJECT_ID);
+console.log("NEW  LINE", process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
+console.log("NEW LINE", process.env.REACT_APP_FIREBASE_MESSAGING_ID);
+console.log("NEW LINE", process.env.REACT_APP_FIREBASE_APP_ID);
 
 function App() {
-  return <div className="app">App</div>;
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <div className="w-screen h-auto flex flex-col">
+        <Header />
+      </div>
+    </AnimatePresence>
+  );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { StateProvider } from "./context/StateProvider";
+import { initialState } from "./context/initialState";
+import reducer from "./context/reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <App />
+      </StateProvider>
+    </Router>
   </React.StrictMode>
 );
