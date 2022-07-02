d2db4669427896332a9624385417e3b6a9067d95
yasirghouri <yasir.ag7@gmail.com>
2022-07-02 21:30:59 +0500

    completed all components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import "./App.css";
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import { Header } from "./components";
+import { CreateContainer, Header, MainContainer } from "./components";
+import { useStateValue } from "./context/StateProvider";
+import { getAllFoodItems } from "./utils/firebaseFunctions";
+import { actionType } from "./context/reducer";
 
-console.log("NEW LINE", process.env.REACT_APP_FIREBASE_API_KEY);
-console.log("NEW LINE", process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
-console.log("NEW DB_URL", process.env.REACT_APP_FIREBASE_DB_URL);
-console.log("NEW LINE", process.env.REACT_APP_FIREBASE_PROJECT_ID);
-console.log("NEW  LINE", process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
-console.log("NEW LINE", process.env.REACT_APP_FIREBASE_MESSAGING_ID);
-console.log("NEW LINE", process.env.REACT_APP_FIREBASE_APP_ID);
+const App = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [{ foodItems }, dispatch] = useStateValue();
 
-function App() {
+  const fetchData = async () => {
+    await getAllFoodItems().then((data) => {
+      dispatch({
+        type: actionType.SET_FOOD_ITEMS,
+        foodItems: data,
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <AnimatePresence exitBeforeEnter>
-      <div className="w-screen h-auto flex flex-col">
+      <div className="w-screen h-auto flex flex-col bg-primary">
         <Header />
+
+        <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+          <Routes>
+            <Route path="/*" element={<MainContainer />} />
+            <Route path="/createItem" element={<CreateContainer />} />
+          </Routes>
+        </main>
       </div>
     </AnimatePresence>
   );
-}
+};
+
 export default App;
